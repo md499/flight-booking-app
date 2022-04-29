3498680bc8c978adc69e8c8bd6b50a93b49c23c0
Minh Q. Le <77988736+lqm181@users.noreply.github.com>
2022-04-29 16:15:54 -0400

    Added documentation for the Servlets.

--- FlightBooking/src/main/java/cs411/flightbooking/servlets/SearchFlightServlet.java
+++ FlightBooking/src/main/java/cs411/flightbooking/servlets/SearchFlightServlet.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package cs411.flightbooking.servlets;
 
 import cs411.flightbooking.dao.FlightDao;
 import cs411.flightbooking.models.Flight;
 import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -17,59 +12,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
- *
- * @author sofi
+ * SearchFlightServlet - a servlet for connecting with the register page
  */
 @WebServlet(name = "SearchFlightServlet", urlPatterns = {"/search-flight", "/user/search-flight"})
 public class SearchFlightServlet extends HttpServlet {
 
-    // initialize the connection between db and web
+    /* Database DAO objects */
     private FlightDao flightdao;
 
     @Override
     public void init() {
         this.flightdao = new FlightDao();
-    }
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet SearchFlightServlet</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>Servlet SearchFlightServlet at " + request.getContextPath() + "</h1>");
-//            out.println("</body>");
-//            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-//        processRequest(request, response);
     }
 
     /**
@@ -83,7 +36,6 @@ public class SearchFlightServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        processRequest(request, response);
         String dLocation = request.getParameter("from");
         String aLocation = request.getParameter("to");
         String Date = request.getParameter("departureTime");
